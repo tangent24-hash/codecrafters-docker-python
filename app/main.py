2ef9473e950f2da253d3f81d2dcf6d3d91c42d9e
Sohan <sohanur471@gmail.com>
2024-05-13 07:23:48 +0600

    isolate file system

--- app/main.py
+++ app/main.py
@@ -1,38 +1,26 @@
-import os
-import subprocess
 import tempfile
+import os
+import shutil
+import subprocess
 import sys
 
-def main():
-    # Remove the print statement for debugging
-    # print("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
+def main():
+
     command = sys.argv[3]
     args = sys.argv[4:]
 
-    # Create a temporary directory for chroot
-    with tempfile.TemporaryDirectory() as tempdir:
-        # Copy the binary to the temporary directory (assuming it's available)
-        try:
-            subprocess.run(["cp", command, tempdir], check=True)
-        except FileNotFoundError:
-            print(f"Error: Could not find binary {command}")
-            sys.exit(1)
+    temp_dir = tempfile.mkdtemp()
+    shutil.copy2(command, temp_dir)
+    os.chroot(temp_dir)
+    command = os.path.join("/", os.path.basename(command))
 
-        # Change root to the temporary directory
-        os.chroot(tempdir)
+    completed_process = subprocess.run([command, *args], capture_output=True)
 
-        # Execute the command with the copied binary path
-        completed_process = subprocess.run(
-            ["./"+command, *args], capture_output=True, check=False)
+    sys.stderr.write(completed_process.stderr.decode("utf-8"))
+    sys.stdout.write(completed_process.stdout.decode("utf-8"))
 
-        # Print both stdout and stderr (decode from bytes)
-        sys.stderr.write(completed_process.stderr.decode("utf-8"))
-        sys.stdout.write(completed_process.stdout.decode("utf-8"))
-
-        # Exit with the same code as the child process
-        sys.exit(completed_process.returncode)
+    sys.exit(completed_process.returncode)
 
 
 if __name__ == "__main__":
