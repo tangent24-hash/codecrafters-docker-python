ef717763c6e95e21a52fe0e7986c586cfd99ff8e
Sohan <sohanur471@gmail.com>
2024-05-13 06:59:12 +0600

    pass wireup stdout stderr

--- app/main.py
+++ app/main.py
@@ -11,9 +11,13 @@ def main():
     command = sys.argv[3]
     args = sys.argv[4:]
     #
-    completed_process = subprocess.run([command, *args], capture_output=True, check=True)
-    print(completed_process.stdout.decode("utf-8"))
-    print(completed_process.stderr.decode("utf-8"), file=sys.stderr)
+    completed_process = subprocess.run(
+        [command, *args], capture_output=True, check=True)
+
+    sys.stderr.write(completed_process.stderr.decode("utf-8"))
+
+    sys.stdout.write(completed_process.stdout.decode("utf-8"))
+
 
 if __name__ == "__main__":
     main()
