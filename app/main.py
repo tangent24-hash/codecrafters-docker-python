98288dc56a1cf7db3e0163d049d94fdb47996c5b
Sohan <sohanur471@gmail.com>
2024-05-13 06:57:30 +0600

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -4,16 +4,16 @@ import sys
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    # print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
     #
     command = sys.argv[3]
     args = sys.argv[4:]
     #
-    completed_process = subprocess.run([command, *args], capture_output=True)
+    completed_process = subprocess.run([command, *args], capture_output=True, check=True)
     print(completed_process.stdout.decode("utf-8"))
-
+    print(completed_process.stderr.decode("utf-8"), file=sys.stderr)
 
 if __name__ == "__main__":
     main()
